dc9cb323cea4567c58ad45677bb8d2f098b13b99
Alejandro Pedreño <5589188@alu.murciaeduca.es>
2024-11-13 10:43:34 +0100

    mi primer commit

---
+++ Videojuego/videojuego.js
@@ -0,0 +1,21 @@
+window.onload = function(){
+
+    const canvas = document.getElementById("miCanvas");
+    const ctx = canvas.getContext("2d");
+    const fondo = new Image();
+    const tronco = new Image();
+
+    fondo.src = "Assets/Images/Fondo_prueba.avif";
+    tronco.src = "Assets/Images/Tronco.png";
+
+    fondo.onload = function() {
+        ctx.drawImage(fondo, 0, 0, 600, 700); 
+    };
+
+    tronco.onload = function(){
+        ctx.drawImage(tronco, 80, 350, 100, 700);
+    }
+
+    
+
+}
