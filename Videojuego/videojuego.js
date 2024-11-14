ad2c272d6ff80b9519b6d84509db138fa12ac927
Alejandro Pedreño <5589188@alu.murciaeduca.es>
2024-11-14 13:23:16 +0100

    Pantalla con opacidad implementada

--- Videojuego/videojuego.js
+++ Videojuego/videojuego.js
@@ -6,6 +6,7 @@ window.onload = function(){
     const tronco = new Image();
     fondo.src = "Assets/Images/Fondo_prueba.avif";
     tronco.src = "Assets/Images/Tronco.png";
+    let troncos = [];                                               //Inicializo array de troncos
 
     fondo.onload = function() {
         ctx.drawImage(fondo, 0, 0, 600, 700); 
@@ -15,12 +16,15 @@ window.onload = function(){
         ctx.drawImage(tronco, 80, 350, 100, 700);
     }
 
-    function iniciarPartida() {
-
+    function iniciarPartida() {                                     //Función al iniciar la partida
         botonIniciar.style.visibility = "hidden";
+        pantallaOpacidad.style.visibility = "hidden";
+    }
 
-        }
-
+    function finPartida(){                                          //Función fin de partida sin usar aún
+        botonIniciar.style.visibility = "shown";
+    }
+    pantallaOpacidad = document.getElementById("overlay");
     botonIniciar = document.getElementById("Iniciarpartida");
     botonIniciar.onclick = iniciarPartida;
 }
